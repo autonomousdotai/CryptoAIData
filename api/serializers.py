d0bb0d857cabcb8187bae7d6731b378301df3f7c
tin <thanhtinpk092007@gmail.com>
2018-05-04 10:49:03 +0700

    Add token balance

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from .models import Profile, Image, Product, Firmware, ImageProfile
+from requests_html import HTMLSession
+session = HTMLSession()
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    token_balance = serializers.SerializerMethodField()
+
+    def get_token_balance(self, obj):
+        r = session.get('https://rinkeby.etherscan.io/token/0x0c3d537e9acad54eb4a5ca297f81e93b9e780373?a=%s' % obj.ether_address)
+        balance = r.html.find('.table', first=True).find('td')[3].text.split()[0]
+        return int(balance)
+
     class Meta:
         model = Profile
         fields = '__all__'
