dccb7cfd5ba74de111417f7d1c8a9d5cadd37462
tin <thanhtinpk092007@gmail.com>
2018-05-04 11:07:11 +0700

    update

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Profile, Image, Product, Firmware, ImageProfile
 from requests_html import HTMLSession
+from django.conf import settings
 session = HTMLSession()
 
 
@@ -10,7 +11,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     def get_token_balance(self, obj):
         balance = 0
         try:
-            r = session.get('https://rinkeby.etherscan.io/token/0x0c3d537e9acad54eb4a5ca297f81e93b9e780373?a=%s' % obj.ether_address)
+            r = session.get('https://rinkeby.etherscan.io/token/%s?a=%s' % (settings.CONTRACT_ADDRESS ,obj.ether_address))
             balance = r.html.find('.table', first=True).find('td')[3].text.split()[0]
         except Exception as err:
             pass
