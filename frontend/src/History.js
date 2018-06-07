8a42b72f47032f824aac2530cb56e0ca628fad7d
Tin <thanhtinpk092007@gmail.com>
2018-06-07 11:11:26 +0700

    [FE] Update UI

--- frontend/src/History.js
+++ frontend/src/History.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {Grid, Image, Container, Dropdown, Card, Icon, Segment, Item, Visibility} from 'semantic-ui-react'
+import {Grid, Image, Container, Card, Form, Segment, Dropdown, Visibility} from 'semantic-ui-react'
 import {AuthConsumer} from './AuthContext'
 import {Route, Redirect} from 'react-router'
 import agent from './agent'
+import {Link} from 'react-router-dom'
 
 
-class History extends React.Component {
+class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.handleUpdate = this.handleUpdate.bind(this);
     this.state = {
+      isLoading: false,
+      img: '',
       images: [],
-      classifies: [],
       nextURL: '',
       calculations: {
         bottomVisible: false,
@@ -18,67 +21,76 @@ class History extends React.Component {
     };
   }
 
-  handleChange = (image, {value}) => {
-    this.setState({value});
-    let classify = value;
-    agent.req.post(agent.API_ROOT + '/api/image-profile/', {classify, image}).set('authorization', `JWT ${this.props.token}`).type('form').then((response) => {
+  componentDidMount() {
+    this.setState({isLoading: true})
+    agent.req.get(agent.API_ROOT + '/api/image-profile/').set('authorization', `JWT ${this.props.token}`).then((response) => {
       let resBody = response.body;
+      this.setState({isLoading: false})
+      this.setState({images: resBody.results, nextURL: resBody.next})
     }).catch((e) => {
     })
   }
 
   handleUpdate = (e, {calculations}) => {
+    let self = this;
+    console.log(calculations)
+    console.log(calculations.percentagePassed)
     this.setState({calculations})
-    if (calculations.bottomVisible) {
-      if (!!this.state.nextURL) {
+    if (calculations.direction === "down" & calculations.percentagePassed > 0.3) {
+      if (!!this.state.nextURL && this.state.isLoading == false) {
+        this.setState({isLoading: true})
         agent.req.get(this.state.nextURL).set('authorization', `JWT ${this.props.token}`).then((response) => {
           let resBody = response.body;
-          let newData = this.state.images.concat(resBody.results)
-          this.setState({images: newData, nextURL: resBody.next})
+          this.setState({isLoading: false})
+          if (resBody.next != self.state.nextURL) {
+            let newData = this.state.images.concat(resBody.results)
+            this.setState({images: newData, nextURL: resBody.next})
+          }
         }).catch((e) => {
         })
       }
     }
   }
 
-  componentDidMount() {
-    agent.req.get(agent.API_ROOT + '/api/image-profile/').set('authorization', `JWT ${this.props.token}`).then((response) => {
-      let resBody = response.body;
-      console.log(resBody);
-      this.setState({images: resBody.results, nextURL: resBody.next})
-    }).catch((e) => {
-    })
-  }
-
-
   render() {
     let self = this;
     return (
-      <Visibility once={true} onUpdate={self.handleUpdate}>
+      <Visibility once={true} onUpdate={this.handleUpdate}>
         <Segment vertical>
-          <Container>
-            <Grid stackable columns={3}>
-              {this.state.images.map(function (item, i) {
-                return (
-                  <Grid.Column key={i}>
-                    <Segment vertical>
-                      <Image src={item.image_url}/>
-                      <h3>{item.category_name} - {item.classify_name}</h3>
-                    </Segment>
-                  </Grid.Column>
-                )
-              })}
-            </Grid>
-          </Container>
+          <div className="ui center aligned grid container">
+            <div className="row">
+              <div className="one wide column"></div>
+              <div className="fourteen wide column">
+                <h1 style={{fontSize: '3rem'}}>Your image classified</h1>
+                <div className="ui three doubling stackable cards" style={{marginTop: "2em"}}>
+                  {this.state.images.map(function (item, i) {
+                    return (
+                      <Card key={i}>
+                        <Image src={item.image_url}/>
+                        <Card.Content>
+                        </Card.Content>
+                        <Card.Content extra>
+                          {item.category_name} - {item.classify}
+                        </Card.Content>
+                      </Card>
+                    )
+                  })}
+                </div>
+              </div>
+              <div className="one wide column"></div>
+            </div>
+          </div>
         </Segment>
+        <Segment vertical loading={this.state.isLoading}/>
       </Visibility>
+
     )
   }
 }
 
 export default props => (<AuthConsumer>
     {({token, isLoading, isAuth}) => {
-      return <History {...props} token={token} isLoading={isLoading} isAuth={isAuth}/>
+      return <Login {...props} token={token} isAuth={isAuth} isLoading={isLoading}/>
     }}
   </AuthConsumer>
 )
