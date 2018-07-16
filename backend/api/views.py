481f74f2c22fc9a9a53936d20ff4a12188911123
Bao Nguyen Hoai <hoaibao@autonomous.nyc>
2018-07-16 10:44:43 +0700

    Refactor

--- backend/api/views.py
+++ backend/api/views.py
@@ -360,7 +360,7 @@ class Feed(generics.ListAPIView):
         fc = profile.following_categories.all()
         fp = profile.following_profiles.all()
         if len(fc) > 0 or len(fp) > 0:
-            queryset = Image.objects.filter(Q(category__in=fc) | Q(profile__in=fp)).order_by('-created')
+            queryset = Image.objects.filter((Q(category__in=fc) | Q(profile__in=fp)) & ~Q(image_profiles__profile=self.request.user.profile)).order_by('-created')
         else:
             queryset = Image.objects.filter(~Q(image_profiles__profile=self.request.user.profile))
         return queryset
