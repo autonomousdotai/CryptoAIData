45dbe0b0f353a3a7d2b32a24097a928a67f0d9f5
Bao Nguyen Hoai <hoaibao@autonomous.nyc>
2018-08-01 18:02:27 +0700

    Add token

--- truffle/migrations/2_deploy_factory.js
+++ truffle/migrations/2_deploy_factory.js
@@ -3,7 +3,7 @@ var DatasetFactory = artifacts.require('./DatasetFactory');
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(DatasetFactory);
   const factory = await DatasetFactory.deployed();
-  await factory.createDataset(accounts[0], 'xx', 'yy', 18, 200);
-  const datasets = await factory.getDatasets.call();
-  console.log('address', datasets);
+  // await factory.createDataset(accounts[0], 'xx', 'yy', 18, 200);
+  // const datasets = await factory.getDatasets.call();
+  // console.log('address', datasets);
 }
--- truffle/test/dataset.js
+++ truffle/test/dataset.js
@@ -24,12 +24,14 @@ contract('Dataset', async (accounts) => {
   const provider2 = accounts[6];
   const provider3 = accounts[7];
 
-  describe('created by provider', async () => {
+  describe('Dataset created by provider', async () => {
     const requestGoal = 0;
 
-    it('dataset properties', async () => {
+    before(async () => {
       this.dataset = await Dataset.new(owner, name, symbol, decimals, requestGoal);
+    });
 
+    it('verify dataset properties', async () => {
       const dsOwner = await this.dataset.owner.call();
       dsOwner.should.be.equal(owner);
 
@@ -39,12 +41,12 @@ contract('Dataset', async (accounts) => {
 
     it('add provider', async () => {
       await this.dataset.addProvider(provider, 1);
-      let tokens = await this.dataset.tokenOf.call(provider);
+      let tokens = await this.dataset.balanceOf.call(provider);
       tokens.should.be.bignumber.equal(1);
 
-      // add one more time
+      // add more tokens
       await this.dataset.addProvider(provider, 2);
-      tokens = await this.dataset.tokenOf.call(provider);
+      tokens = await this.dataset.balanceOf.call(provider);
       tokens.should.be.bignumber.equal(3);
 
       const providers = await this.dataset.getProviders.call();
@@ -62,7 +64,7 @@ contract('Dataset', async (accounts) => {
       let balance = await this.dataset.balance.call();
       balance.should.be.bignumber.equal(utils.toWei(0.9, 'ether'));
 
-      // one more buy
+      // buy one more time
       value = utils.toWei(2, 'ether');
       await this.dataset.buy({value, from: buyer2});
 
@@ -83,40 +85,37 @@ contract('Dataset', async (accounts) => {
 
       const dsBalance = await this.dataset.balance.call();
       const currentQuantity = await this.dataset.currentQuantity.call();
-      let tokens, providerBalance, expected, percent;
+      let tokens, beforePaid, afterPaid, expected;
 
       // calculate first provider tokens
-      tokens = await this.dataset.tokenOf.call(provider);
-      percent = tokens.div(currentQuantity);
-      providerBalance = await web3.eth.getBalance(provider);
-      expected = providerBalance.add(dsBalance.mul(percent));
+      tokens = await this.dataset.balanceOf.call(provider);
+      beforePaid = await web3.eth.getBalance(provider);
+      expected = beforePaid.add(tokens.mul(dsBalance).div(currentQuantity));
       // paid for first provider
-      await this.dataset.paid(provider);
+      await this.dataset.paid(provider, tokens);
       // verify balance after paid
-      providerBalance = await web3.eth.getBalance(provider);
-      providerBalance.should.be.bignumber.equal(expected);
+      afterPaid = await web3.eth.getBalance(provider);
+      afterPaid.should.be.bignumber.equal(expected);
 
       // calculate second provider tokens
-      tokens = await this.dataset.tokenOf.call(provider2);
-      percent = tokens.div(currentQuantity);
-      providerBalance = await web3.eth.getBalance(provider2);
-      expected = providerBalance.add(dsBalance.mul(percent));
+      tokens = await this.dataset.balanceOf.call(provider2);
+      beforePaid = await web3.eth.getBalance(provider2);
+      expected = beforePaid.add(tokens.mul(dsBalance).div(currentQuantity));
       // paid for second provider
-      await this.dataset.paid(provider2);
+      await this.dataset.paid(provider2, tokens);
       // verify balance after paid
-      providerBalance = await web3.eth.getBalance(provider2);
-      providerBalance.should.be.bignumber.equal(expected);
+      afterPaid = await web3.eth.getBalance(provider2);
+      afterPaid.should.be.bignumber.equal(expected);
 
       // calculate third provider tokens
-      tokens = await this.dataset.tokenOf.call(provider3);
-      percent = tokens.div(currentQuantity);
-      providerBalance = await web3.eth.getBalance(provider3);
-      expected = providerBalance.add(dsBalance.mul(percent));
+      tokens = await this.dataset.balanceOf.call(provider3);
+      beforePaid = await web3.eth.getBalance(provider3);
+      expected = beforePaid.add(tokens.mul(dsBalance).div(currentQuantity));
       // paid for third provider
-      await this.dataset.paid(provider3);
+      await this.dataset.paid(provider3, tokens);
       // verify balance after paid
-      providerBalance = await web3.eth.getBalance(provider3);
-      providerBalance.should.be.bignumber.equal(expected);
+      afterPaid = await web3.eth.getBalance(provider3);
+      afterPaid.should.be.bignumber.equal(expected);
     });
 
     it('withdraw owner balance', async () => {
@@ -129,13 +128,94 @@ contract('Dataset', async (accounts) => {
     });
   });
 
-  // describe('Dataset created by buyer', async () => {
-  //   it('created by buyer', async () => {
-  //     const requestGoal = 1;
-  //
-  //     const instance = await Dataset.new(accounts[0], name, symbol, decimals, requestGoal);
-  //     const createdBy = await instance.createdBy.call();
-  //     createdBy.should.be.bignumber.equal(1);
-  //   });
-  // })
+  describe('Dataset created by buyer', async () => {
+    const requestGoal = 5;
+
+    before(async () => {
+      this.dataset = await Dataset.new(accounts[0], name, symbol, decimals, requestGoal);
+    });
+
+    it('verify dataset properties', async () => {
+      const dsOwner = await this.dataset.owner.call();
+      dsOwner.should.be.equal(owner);
+
+      const createdBy = await this.dataset.createdBy.call();
+      createdBy.should.be.bignumber.equal(1);
+    });
+
+    it('requester send eth to created dataset', async () => {
+      let value = utils.toWei(1, 'ether');
+      await this.dataset.request({value, from: requester});
+
+      // verify requester
+      const dsRequester = await this.dataset.requester.call();
+      dsRequester.should.be.equal(requester);
+
+      let dsRequestAmount = await this.dataset.requestAmount.call();
+      dsRequestAmount.should.be.bignumber.equal(utils.toWei(1, 'ether'));
+
+      // one more request
+      value = utils.toWei(4, 'ether');
+      await this.dataset.request({value, from: requester});
+
+      dsRequestAmount = await this.dataset.requestAmount.call();
+      dsRequestAmount.should.be.bignumber.equal(utils.toWei(5, 'ether'));
+    });
+
+    it('paid for providers', async () => {
+      // add provider
+      await this.dataset.addProvider(provider, 1);
+
+      // try to paid when the dataset hasn't reach goal yet
+      let beforePaid = await web3.eth.getBalance(provider);
+      await this.dataset.paid(provider, 1);
+      let afterPaid = await web3.eth.getBalance(provider);
+      afterPaid.should.be.bignumber.equal(beforePaid);
+
+      // add more providers to make the goal reached
+      await Promise.all([
+        this.dataset.addProvider(provider2, 1),
+        this.dataset.addProvider(provider3, 3),
+      ]);
+
+      const currentQuantity = await this.dataset.currentQuantity.call();
+
+      // paid for first provider
+      let tokens = await this.dataset.balanceOf.call(provider);
+      beforePaid = await web3.eth.getBalance(provider);
+      await this.dataset.paid(provider, tokens);
+
+      // owner keeps 10% by default
+      const ownerBalance = await this.dataset.ownerBalance.call();
+      ownerBalance.should.be.bignumber.equal(utils.toWei(0.5, 'ether'));
+
+      const dsBalance = await this.dataset.balance.call();
+      dsBalance.should.be.bignumber.equal(utils.toWei(4.5, 'ether'));
+
+      // the request amount should be 0 now
+      const requestAmount = await this.dataset.requestAmount.call();
+      requestAmount.should.be.bignumber.equal(0);
+
+      // verify provider balance
+      expected = beforePaid.add(tokens.mul(dsBalance).div(currentQuantity));
+      afterPaid = await web3.eth.getBalance(provider);
+      afterPaid.should.be.bignumber.equal(expected);
+
+      // paid for second provider
+      tokens = await this.dataset.balanceOf.call(provider2);
+      beforePaid = await web3.eth.getBalance(provider2);
+      expected = beforePaid.add(tokens.mul(dsBalance).div(currentQuantity));
+      await this.dataset.paid(provider2, tokens);
+      afterPaid = await web3.eth.getBalance(provider2);
+      afterPaid.should.be.bignumber.equal(expected);
+
+      // paid for third provider
+      tokens = await this.dataset.balanceOf.call(provider3);
+      beforePaid = await web3.eth.getBalance(provider3);
+      expected = beforePaid.add(tokens.mul(dsBalance).div(currentQuantity));
+      await this.dataset.paid(provider3, tokens);
+      afterPaid = await web3.eth.getBalance(provider3);
+      afterPaid.should.be.bignumber.equal(expected);
+    });
+  });
 });
