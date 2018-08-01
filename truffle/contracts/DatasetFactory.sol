9658146703b29209742910670b4fb3f8be2d72d3
Bao Nguyen Hoai <hoaibao@autonomous.nyc>
2018-08-01 10:10:16 +0700

    Refactor

--- truffle/contracts/DatasetFactory.sol
+++ truffle/contracts/DatasetFactory.sol
@@ -7,9 +7,11 @@ contract DatasetFactory is Ownable {
 
   address[] public datasets;
 
-  function createDataset(address owner, string name, string symbol, uint8 decimals, uint256 requestGoal) onlyOwner public {
-    Dataset ds = new Dataset(owner, name, symbol, decimals, requestGoal);
+  function createDataset(address owner, string name, string symbol, uint8 decimals, uint256 requestGoal) onlyOwner public returns (address) {
+    address ds = new Dataset(owner, name, symbol, decimals, requestGoal);
     datasets.push(ds);
+
+    return ds;
   }
 
   function getDatasets() public view returns (address[]) {
