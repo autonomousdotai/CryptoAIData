b3cc892a5a6cea728ace0ee0b3b03d4a07f764e4
Moehau <binh@autonomous.nyc>
2018-08-06 13:43:55 +0700

    dleete

--- backend/cron_jobs/verify_classification/env_vars.sh
+++
@@ -1,7 +0,0 @@
-export MYSQL_HOST=35.198.219.109
-export MYSQL_USER=root
-export MYSQL_PASSWORD=nygumZw4aRam
-export MYSQL_DB=trashcan_dev
-export LABEL_NUM=2
-export API_HOST=http://192.168.0.235
-export API_PORT=8000
